efe603afdec440ce556990e9171ae41c479665ac
dm3mak <ianakazantipa@gmail.com>
2021-05-10 10:48:12 +0300

    added Active user State,condition render/

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Login from './pages/Login'
 import RecipesPage from './pages/RecipesPage'
 import Signup from './pages/Signup'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RecipesNavbar from './components/RecipesNavbar'
-
+//App is the main component for our Recipe app
+// State:
+// activeUser: Either null , if
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      //activeUser: null
+      activeUser: {
+        id: 1,
+        name: 'Yaron',
+        email: 'skarlinski@gmail.com',
+        pwd: '123'
+      }
+    }
+  }
+  logout = () => {
+    this.setState ({
+      activeUser: null
+    })
+  }
   render() {
     return (
       <div>
-        <RecipesNavbar></RecipesNavbar>
         <HashRouter>
+          <Route  exact path ={["/", "/recipes"]}>
+            <RecipesNavbar 
+            activeUser = {this.state.activeUser}
+            logout = {this.logout}
+            ></RecipesNavbar>
+          </Route>
           <Route  exact path ="/">
             <HomePage></HomePage>
           </Route>
--- src/components/RecipesNavbar.js
+++ src/components/RecipesNavbar.js
@@ -2,16 +2,26 @@ import React, { Component } from 'react'
 import { Nav, Navbar } from 'react-bootstrap'
 
 export default class RecipesNavbar extends Component {
+    constructor(props){
+        super(props);
+    }
     render() {
+        const loginEl =  (!this.props.activeUser)? <Nav.Link href="/#/login">Login</Nav.Link>: null
+        const signupEl = (!this.props.activeUser)?<Nav.Link href="/#/signup">Signup</Nav.Link>: null
+        const logoutEl = (this.props.activeUser)? <Nav.Link href="/#/" onClick={() => this.props.logout()}>Logout</Nav.Link>: null
+
         return (
             <Navbar bg="light" expand="lg">
                 <Navbar.Brand href="/#/">My Recipe Book</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto">
+                    <Nav className="mr-auto">
                         <Nav.Link href="/#/recipes">recipes</Nav.Link>
-                        <Nav.Link href="/#/login">Login</Nav.Link>
-                        <Nav.Link href="/#/signup">Signups</Nav.Link>    
+                    </Nav>
+                    <Nav className="ml-auto">
+                        {loginEl}
+                        {signupEl}  
+                        {logoutEl}
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
